a5dd04ef6779433e8bacc973c03452ba461e4731
mr0jue <15510272938@163.com>
2018-08-29 12:31:57 +0800

    提取tfidf过程
    提取tfidf过程，结果保存为矩阵文件，可以被其他程序直接调用

--- tfidf.py
+++ tfidf.py
@@ -1,16 +1,21 @@
-import time, pandas as pd, numpy as np
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+# 对文档执行TF-IDF过程，并保存结果
+import pandas as pd
+from scipy import sparse
+from time import time
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-t1=time.time()
+t1=time()
 
 train = pd.read_csv('../data/train_set.csv')
 test = pd.read_csv('../data/test_set.csv')
-test_id = test["id"].copy()
 column="word_seg"
 
-n = train.shape
-# 将原始文档集合转换为TF-IDF特征矩阵
 vec = TfidfVectorizer(ngram_range=(1,2),min_df=3, max_df=0.9,use_idf=1,smooth_idf=1, sublinear_tf=1)
-# 这一步的变换是比较耗时的。
 trn_term_doc = vec.fit_transform(train[column])
 test_term_doc = vec.transform(test[column])
+
+t2=time()
+print("tfidf time use:",t2-t1)
+
+sparse.save_npz('./trn_term_doc.npz', trn_term_doc) 
+sparse.save_npz('./test_term_doc.npz', test_term_doc) 
